b487055b091edfd726855a053a5c4d1313c57ff0
xiuyuandashen <1556450877@qq.com>
2021-07-10 11:59:58 +0800

    xxx

--- src/main.js
+++ src/main.js
@@ -20,10 +20,11 @@ Vue.use(elementUtils);
 
 import tinymce from 'tinymce'
 import VueTinymce from '@packy-tang/vue-tinymce'
+import * as echarts from 'echarts';
 
 Vue.prototype.$tinymce = tinymce // 将全局tinymce对象指向给Vue作用域下
 Vue.use(VueTinymce)              // 安装vue的tinymce组件
-
+Vue.prototype.$echarts = echarts
 
 Vue.use(ElementUI, { locale })
 
--- src/views/dashboard/index.vue
+++ src/views/dashboard/index.vue
@@ -1,81 +1,67 @@
 <template>
   <div class="dashboard-container">
-
-    <el-card class="box-card" >
-      <div slot="header" class="clearfix" >
-        <h2>欢迎，{{name}} 使用优社管理系统 &nbsp <span style="font-size:12px;font-weight:200">您的用户角色 roles:<span v-for="role in roles" :key="role">{{ role }}</span></span></h2> 
-        <!-- {{time}} -->
+    <el-card class="box-card">
+      <div slot="header" class="clearfix">
+        <h2>
+          欢迎，{{ name }} 使用优社管理系统 &nbsp
+          <span style="font-size: 12px; font-weight: 200"
+            >您的用户角色 roles:<span v-for="role in roles" :key="role"
+              >{{ role }}
+            </span></span
+          >
+        </h2>
       </div>
-        <div>
-          
-    
-        </div>
+      <div class="a" style="width: 600px; height: 300px"></div>
     </el-card>
-    
-    
   </div>
 </template>
 
 <script>
-import { mapGetters } from 'vuex'
+import { mapGetters } from "vuex";
 
 export default {
-  name: 'Dashboard',
-  data(){
+  name: "Dashboard",
+  data() {
     return {
       // time:new Date(),
       // timeString:""
-    }
+    };
   },
-  mounted(){
-    // let _this = this; // 声明一个变量指向Vue实例this，保证作用域一致
-    // this.timer = setInterval(() => {
-    //   _this.time =  new Date(); // 修改数据date
-    // }, 1000)
+  mounted() {
+    this.initChart();
   },
   computed: {
-    ...mapGetters([
-      'name',
-      'roles'
-    ]),
-    // timeString(){
-    //   return this.getTime(this.time)
-    // }
-
+    ...mapGetters(["name", "roles"]),
   },
-  watch:{
-    // time: function(newTime,oldTime){
-    //   this.timeString = this.getTime(newTime)
-    //   console.log(this.timeString)
-    // }
-  }
-  ,
-  created(){
-    
+  watch: {},
+  created() {},
+  methods: {
+    initChart() {
+      console.log(this.$echarts);
+      let mychart = this.$echarts.init(document.querySelector(".a"));
+      mychart.setOption({
+        title: {
+          text: "ECharts 入门示例",
+        },
+        tooltip: {},
+        legend: {
+          data: ["销量"],
+        },
+        xAxis: {
+          data: ["衬衫", "羊毛衫", "雪纺衫", "裤子", "高跟鞋", "袜子"],
+        },
+        yAxis: {},
+        series: [
+          {
+            name: "销量",
+            type: "bar",
+            data: [5, 20, 36, 10, 10, 20],
+          },
+        ],
+      });
+    },
   },
-  methods:{
-    getTime(now){
-        
-        // let now = date;
-        let year = now.getFullYear(); //获取年份
-        let month = now.getMonth(); //获取月份
-        let date = now.getDate(); //获取日期
-        let day = now.getDay(); //获取星期
-        let hour = now.getHours(); //获取小时
-        let minute = now.getMinutes(); //获取分钟
-        let seconds = now.getSeconds(); //获取秒
-        month = month + 1;
-        let arr_week = new Array("星期日", "星期一", "星期二", "星期三", "星期四", "星期五", "星期六");
-        let week = arr_week[day];
-        return time = year + "年" + month + "月" + date + "日 " + week + " " + hour + ":" + minute + ":" + seconds;
-    }
-  },
-   beforeDestroy() {
-    if (this.timer) {
-      clearInterval(this.timer); // 在Vue实例销毁前，清除我们的定时器
-    }
-  }
-}
+};
 </script>
 
 <style rel="stylesheet/scss" lang="scss" scoped>
